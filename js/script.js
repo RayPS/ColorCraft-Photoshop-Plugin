fc5afed3e76265d0df2097e6dede6edf9e28b355
Ray <ray@rayps.com>
2015-02-08 16:48:04 +0800

    Hue

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ var myExtensionId = "colorshading"
 
 function init() {
 	themeManager.init();
-	Persistent(false)
+	Persistent(true)
 
 	spectrum(false)
 
@@ -55,16 +55,85 @@ function spectrum(isBackground) {
 $("main").on('mousewheel', function(event){
     return false;
 }).onepage_scroll({
-	animationTime: 500,
+	animationTime: 250,
 	direction: "horizontal"
 })
 
-
-$("#colorpicker").mouseup(function(event) {
+$("#colorpicker").on('mouseup mouseleave', function(event) {
 	var color = $("#spectrum").spectrum("get").toHex()
+	generate(color)
+
 	if (event.altKey) {
 		csInterface.evalScript("app.backgroundColor.rgb.hexValue = '" + color + "'")
 	} else {
 		csInterface.evalScript("app.foregroundColor.rgb.hexValue = '" + color + "'")
-	}
+	}	
 })
+
+
+function generate (c) {
+
+	for (var i = 0; i < 7; i++) {
+		$(".T li")[i].style.backgroundColor = palette.t(c)[i]
+		$(".H li")[i].style.backgroundColor = palette.h(c)[i]
+	}
+}
+
+var palette = {
+	t: function(baseColor){
+		var palette = new Array()
+		var gap = 20
+
+		for (var i = 0; i < 7; i++) {
+		    if (i < 4) {
+		        palette[i] = tinycolor.mix(baseColor, tinycolor("Blue") , ~(i - 4) * 0.15)
+		                .darken(~(i - 4) * 0.15)
+		                .saturate(~(i - 4) * 0.3)
+		                .toHexString()
+		    } else{
+		        palette[i] = tinycolor.mix(baseColor, tinycolor("Gold") , i * 0.06)
+		                .lighten(i * 0.075)
+		                .saturate(i * 0.01)
+		                .toHexString()
+		    }
+		}
+
+		return palette;
+	},
+	
+	h: function(baseColor){
+		var palette = new Array()
+		var gap = 20
+
+		for (var i = 0; i < 7; i++) {
+			palette[i] = tinycolor(baseColor).spin((i - 3) * gap)
+		}
+		return palette  
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
