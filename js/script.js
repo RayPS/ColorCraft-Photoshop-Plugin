13de39399e13ebcc499d7a5bead57f6d0fdbe8c0
Ray <ray@rayps.com>
2015-02-09 14:05:31 +0800

    fix something and improve logic

--- js/script.js
+++ js/script.js
@@ -19,7 +19,16 @@ function init() {
 	themeManager.init();
 	Persistent(true)
 
-	spectrum(false)
+	getPSColor(false, function(callback){
+		$("#spectrum").spectrum({
+		    color: callback,
+		    flat: true,
+		    clickoutFiresChange: true,
+		    showButtons: false
+		})
+
+		pick(callback,true)
+	})
 
 	document.oncontextmenu = function() {return false;};
 }
@@ -27,17 +36,17 @@ init();
 
 
 var palette = {
-	t: function(baseColor){
+	t: function(color){
 		var C = new Array()
 
 		for (var i = 0; i < 7; i++) {
 		    if (i < 4) {
-		        C[i] = tinycolor.mix(baseColor, tinycolor("Blue") , ~(i - 4) * 20)
+		        C[i] = tinycolor.mix(color, tinycolor("Blue") , ~(i - 4) * 20)
 		        				.darken(~(i - 4) * 5)
 		        				.desaturate(~(i - 4) * 3)
 		                		.toHexString()
 		    } else{
-		        C[i] = tinycolor.mix(baseColor, tinycolor("Yellow") , i * 6)
+		        C[i] = tinycolor.mix(color, tinycolor("Yellow") , i * 6)
 		        				.brighten(i * 3)
 		            		    .toHexString()
 		    }
@@ -46,42 +55,42 @@ var palette = {
 		return C;
 	},
 
-	b: function(baseColor){
+	b: function(color){
 		var C = new Array()
 		var gap = 10
 
 		for (var i = 0; i < 7; i++) {
-			C[i] = tinycolor(baseColor).brighten((i - 3) * gap)
+			C[i] = tinycolor(color).brighten((i - 3) * gap)
 		}
 		return C 
 	},
 
-	l: function(baseColor){
+	l: function(color){
 		var C = new Array()
 		var gap = 10
 
 		for (var i = 0; i < 7; i++) {
-			C[i] = tinycolor(baseColor).lighten((i - 3) * gap)
+			C[i] = tinycolor(color).lighten((i - 3) * gap)
 		}
 		return C 
 	},
 
-	s: function(baseColor){
+	s: function(color){
 		var C = new Array()
 		var gap = 10
 
 		for (var i = 0; i < 7; i++) {
-			C[i] = tinycolor(baseColor).saturate((i - 3) * gap)
+			C[i] = tinycolor(color).saturate((i - 3) * gap)
 		}
 		return C 
 	},
 
-	h: function(baseColor){
+	h: function(color){
 		var C = new Array()
 		var gap = 10
 
 		for (var i = 0; i < 7; i++) {
-			C[i] = tinycolor(baseColor).spin((i - 3) * gap)
+			C[i] = tinycolor(color).spin((i - 3) * gap)
 		}
 		return C  
 	}
@@ -99,7 +108,7 @@ function Persistent(inOn) {
     csInterface.dispatchEvent(event);
 }
 
-function spectrum(isBackground) {
+function getPSColor(isBackground, doSomething){
 	var script
 	if (isBackground) {
 		script = "app.backgroundColor.rgb.hexValue"
@@ -108,14 +117,7 @@ function spectrum(isBackground) {
 	}
 
 	csInterface.evalScript(script ,function(callback){
-		$("#spectrum").spectrum({
-		    color: callback,
-		    flat: true,
-		    clickoutFiresChange: true,
-		    showButtons: false
-		})
-
-		generate(callback)
+		doSomething(callback)
 	})
 }
 
@@ -129,20 +131,31 @@ function setPSColor(color, isBackground) {
 	csInterface.evalScript(script)
 }
 
-function hexColor(){
-	return $("#spectrum").spectrum("get").toHex()
+function getSpectrum(){
+	return $("#spectrum").spectrum("get")
 }
 
-
-function generate (c) {
+function generate (color) {
 
 	for (var i = 0; i < 7; i++) {
-		$(".T li")[i].style.backgroundColor = palette.t(c)[i]
-		$(".B li")[i].style.backgroundColor = palette.b(c)[i]
-		$(".L li")[i].style.backgroundColor = palette.l(c)[i]
-		$(".S li")[i].style.backgroundColor = palette.s(c)[i]
-		$(".H li")[i].style.backgroundColor = palette.h(c)[i]
+		$(".T li")[i].style.backgroundColor = palette.t(color)[i]
+		$(".B li")[i].style.backgroundColor = palette.b(color)[i]
+		$(".L li")[i].style.backgroundColor = palette.l(color)[i]
+		$(".S li")[i].style.backgroundColor = palette.s(color)[i]
+		$(".H li")[i].style.backgroundColor = palette.h(color)[i]
 	}
+}
+
+function pick(color, regen, isBackground){
+	
+	$(".selected").removeClass('selected')
+	setPSColor(tinycolor(color).toHex(), isBackground)
+	$("#spectrum").spectrum("set", color)
+	
+	regen && generate(color)
+
+	var style = 'style="background-color: ' + tinycolor(color).toRgbString() + ';"'
+	$("li[" + style + "]").addClass('selected')
 }
 
 
@@ -154,44 +167,26 @@ $("main").on('mousewheel', function(event){
 })
 
 $("#colorpicker").on('mouseup mouseleave', function(event) {
-	setPSColor(hexColor(), event.altKey)
+
+	pick(getSpectrum().toHex(), true, event.altKey)
+	
 }).on("move.spectrum", function(event){
-	generate(hexColor())
+	generate(getSpectrum().toHex())
 })
 
 
 $("#shading li").mousedown(function(event) {
 
 	var isRightClick = event.button == 2
-
-	pick(this, isRightClick)
+	var color = $(this).css("background-color")
+	pick(color, isRightClick)
 
 }).dblclick(function(event) {
 
-	pick(this,true)
+	var color = $(this).css("background-color")
+	pick(color,true)
 
 })
-
-
-function pick(ele, select){
-
-	var bgc = $(ele).css("background-color")
-	var style = 'style="background-color: ' + bgc + ';"'
-	
-	$(".selected").removeClass('selected')
-	setPSColor(tinycolor(bgc).toHex())
-	$("#spectrum").spectrum("set", bgc)
-	
-	if (select) {
-		generate(bgc)
-	}
-	
-	$("li[" + style + "]").addClass('selected')
-}
-
-
-var r = function(){location.reload()}
-
 
 
 
