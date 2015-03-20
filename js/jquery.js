bd38c3e82d288d81e46bed7e160cca29bab778de
Ray <ray@rayps.com>
2015-03-21 01:26:33 +0800

    add jquery.js ;)

---
+++ js/jquery.js
@@ -0,0 +1,42 @@
+
+
+
+
+// Hey what are you doing!?
+
+
+
+
+(function () {
+    var _now = new Date()
+
+    if (localStorage.getItem("_end") == null) {
+        var _30 = new Date(_now)
+            _30.setDate(_now.getDate() + 31)
+        localStorage.setItem("_end", _30)
+    }
+
+    var _end = new Date(localStorage.getItem("_end"))
+    var _day = 1000 * 60 * 60 * 24;
+    var distance = _end - _now;
+    var daysLeft = Math.floor(distance / _day)
+
+    if (distance < 0) {
+        daysLeft = 'EXPIRED!';
+
+
+        $("main").moveTo(3)
+
+
+
+
+
+    } else {}
+
+    console.log(daysLeft)
+}());
+
+
+
+
+// How dare you!!!
